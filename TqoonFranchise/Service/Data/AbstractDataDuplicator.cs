e528166214afe551e86e00e753f770f7b89212de
abraham <jsabraham.miny@gmail.com>
2020-11-06 11:24:33 +0900

    반쯤 했음

--- TqoonFranchise/Service/Data/AbstractDataDuplicator.cs
+++ TqoonFranchise/Service/Data/AbstractDataDuplicator.cs
@@ -13,37 +13,6 @@ namespace TqoonFranchise.Service.Data
     {
         public ICommonObjectDao MCod { get; set; }
         public ICommonObjectDao TCod { get; set; }
-        //public FranchiseParam Param { get; set; }
-        //public string SelectQuery { get; set; }
-        //public IList<CopyItem<T>> DoIt() {
-        //    IList<CopyItem<T>> list = new List<CopyItem<T>>();
-        //    IList<T> modelItems = GetModelItems();
-        //    foreach(T mItem in modelItems)
-        //    {
-        //        CopyItem<T> copyItem = new CopyItem<T>();
-        //        copyItem.Model = mItem;
-        //        copyItem.Target = InsertTarget(mItem);
-        //        list.Add(copyItem);
-        //    }
-        //    return list;
-        //}
-       
-        //protected T InsertTarget(T mItem)
-        //{
-            
-        //    int id =TCod.Insert<T>(mItem);
-        //    return mItem;            
-        //}
-
-        //protected IList<T> GetModelItems()
-        //{
-        //    string tableName = CodQueryUtil.get
-        //    return TCod.Query<T>(new ListQuery<T>
-        //    {
-        //        Query = "",
-        //        DbParam = null
-
-        //    });
-        //}
+        
     }
 }
--- TqoonFranchise/Service/Data/PartnerPageContentsDuplicator.cs
+++ TqoonFranchise/Service/Data/PartnerPageContentsDuplicator.cs
@@ -1,4 +1,5 @@
 ﻿using Adprint.PartnerPageContents.Model;
+using JangBoGo.Info.Object;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,11 +9,35 @@ using TqoonFranchise.Model;
 
 namespace TqoonFranchise.Service.Data
 {
-    public class PartnerPageContentsDuplicator
+    public class PartnerPageContentsDuplicator : AbstractDataDuplicator
     {
-        public IList<CopyItem<PartnerPageContentsItem>> DoIt(int mJoiner, int tJoiner)
+        public IList<CopyItem<PartnerPageContentsItem>> DoIt(int mJoinerId, int tJoinerId)
         {
-            throw new NotImplementedException();
+            var mList = GetMList(mJoinerId);
+            var cList = new List<CopyItem<PartnerPageContentsItem>>();
+            foreach (var mItem in mList)
+            {
+                var tItem = mItem.Clone<PartnerPageContentsItem>();
+                tItem.JoinerId = tJoinerId;
+                tItem.Id = TCod.Insert<PartnerPageContentsItem>(tItem);
+
+                var cItem = new CopyItem<PartnerPageContentsItem>()
+                {
+                    Model = mItem,
+                    Target = tItem
+                };
+                cList.Add(cItem);
+            }
+            return cList;
+        }
+        private IList<PartnerPageContentsItem> GetMList(int joinerId)
+        {
+            string query = "SELECT * FROM PartnerPageContents WHERE state='REG' AND joinerId=@joinerId";
+            return MCod.Query<PartnerPageContentsItem>(new ListQuery<PartnerPageContentsItem>
+            {
+                Query = query,
+                DbParam = new { joinerId }
+            });
         }
     }
 }
