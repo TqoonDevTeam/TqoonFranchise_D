372bc353121db59c61003504a4d6010e1961d655
abraham <jsabraham.miny@gmail.com>
2020-11-06 14:30:16 +0900

    aaa

--- TqoonFranchise/Service/Data/GoodsPriceDuplicator.cs
+++ TqoonFranchise/Service/Data/GoodsPriceDuplicator.cs
@@ -2,15 +2,36 @@
 using Adprint.GoodsPrice.Model;
 using Adprint.PartnerGoodsPrice.Model;
 using System.Collections.Generic;
+using System.Linq;
 using TqoonFranchise.Model;
 
 namespace TqoonFranchise.Service.Data
 {
-    public class GoodsPriceDuplicator
+    public class GoodsPriceDuplicator: AbstractDataDuplicator
     {
-        public IList<CopyItem<GoodsPriceItem>> DoIt(IList<CopyItem<GoodsItem>> CGoodsItems, IList<CopyItem<PartnerGoodsPriceItem>> CPartnerGoodsPriceItems)
+        public IList<CopyItem<GoodsPriceItem>> DoIt(IList<CopyItem<GoodsItem>> relatedCGoodsList, 
+                                                    IList<CopyItem<PartnerGoodsPriceItem>> relatedCPartnerGoodsPriceList)
         {
-            return null;
+            var relatedMGoodsList = relatedCGoodsList.Select(t => t.Model).ToList();
+            IList<CopyItem<GoodsPriceItem>> cList = new List<CopyItem<GoodsPriceItem>>();
+            IList<GoodsPriceItem> mList = GetMList(relatedMGoodsList);
+
+            foreach (var mItem in mList)
+            {
+                var relatedCItem = relatedCList.Where(t => t.Model.Id == mItem.CalcInfoId).Single();
+                var tItem = mItem.Clone<CalcDataItem>();
+                tItem.CalcInfoId = relatedCItem.Target.Id;
+                tItem.OwnedJoinerId = tJoinerId;
+                tItem.Id = TCod.Insert<CalcDataItem>(tItem);
+
+                var cItem = new CopyItem<CalcDataItem>()
+                {
+                    Model = mItem,
+                    Target = tItem
+                };
+                cList.Add(cItem);
+            }
+            return cList;
         }
     }
 }
